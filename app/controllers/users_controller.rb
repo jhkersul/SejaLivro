9e1e6f5b12fdcd5c63861415e39863aac8ca830c
Kersul <joaohkfaria@gmail.com>
2016-06-15 02:14:05 -0300

    Atualizando arquivos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    # Convertendo genero inteiro
+    params[:user][:gender] = params[:user][:gender].to_i
+
+    # Build nos parametros
     @user = User.new(user_params)
     @user.addresses = []
 
@@ -18,10 +22,10 @@ class UsersController < ApplicationController
       if @user.save
         @user.addresses.build(user_params[:addresses_attributes][0])
         @user.save
-        format.html { redirect_to action: :index, notice: 'User created' }
-        format.json { render :index, status: :created, location: @user }
+        format.html { redirect_to root_path, notice: 'User created' }
+        format.json { render root_path, status: :created, location: @user }
       else
-        format.html { render :register }
+        format.html { render :cadastro }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -30,6 +34,6 @@ class UsersController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :cpf, :login, :birth_date, :password, :password_confirmation, :gender, :addresses_attributes => [ :zipcode, :city, :country, :state, :complement, :street, :quarter])
+      params.require(:user).permit(:name, :cpf, :email, :login, :birth_date, :gender, :password, :password_confirmation, :addresses_attributes => [ :zipcode, :city, :country, :state, :complement, :street, :quarter])
     end
 end
