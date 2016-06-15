f623871c7b32a12857a1e4578cb4bf1dc2b2fc8b
lumyharada <lumy.harada@gmail.com>
2016-06-15 17:48:41 -0300

    CRUD livros

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,16 @@ class BooksController < ApplicationController
     @categories = Category.all
   end
 
+  def index
+    @books = Book.all
+  end
+
+  def edit
+    if @book.nil?
+      redirect_to books_path
+    end
+  end
+
   # POST /users
   # POST /users.json
   def create
@@ -12,7 +22,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to action: :new, notice: 'Book created' }
+        format.html { redirect_to action: :index, notice: 'Book created' }
         format.json { render :new, status: :created, location: @book }
       else
         format.html { render :new }
@@ -21,9 +31,28 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to action: :index, notice: 'Book updated' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    if book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+    end
+
+  end
   private
   # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
+  def book_params
     params.require(:book).permit(:name, :isbn, :author, :language, :publisher, :year)
   end
 
