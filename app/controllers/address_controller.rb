84a6464c895f199d9a5c35a5c4677b1acb1026b8
Rafix02 <rafixmeneses@gmail.com>
2016-06-21 12:19:31 -0300

    Atualiza user, endereço, algumas routes mudaram e coisas no profile

---
+++ app/controllers/address_controller.rb
@@ -0,0 +1,30 @@
+class AddressController < ApplicationController
+
+  def edit
+  	@address = User.find(params[:id]).addresses.first
+
+  	if @address.nil?
+  		redirect_to profile_path
+  	end
+  end
+
+  def update
+  	@address = User.find(params[:id]).addresses.first
+
+  	respond_to do |format|
+      if @address.update(address_params)
+        format.html { redirect_to profile_path, notice: 'Atualizado com sucesso!' }
+        format.json { render action: :profile, status: :created, location: @user }
+      else
+        format.html { render profile_path }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+  def address_params
+  	params.require(:address).permit(:zipcode, :city, :country, :state, :complement, :street, :quarter)
+  end
+end
